8173e5cd14f6c9e49fd06cb73bb57a28a78b31ed
Adrien Rosi <adrienrosi@gmail.com>
2023-10-04 09:53:58 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Cee Connect - Client Swagger
+
+⚠️ Version de démonstration
+
+Catalogue API pour la gestion des opérations standardisées d'économies d'énergie dans le cadre du dispositif des Certificats d'Economies d'Energie.
+
+## Usage
+
+`
+git clone https://github.com/CeeConnect/apis-doc.git path/to/directory
+npm install
+npm run dev
+`
+
+## Licence
+
+[CC-BY-NC-SA](http://creativecommons.org/licenses/by-nc-sa/4.0/)
---
+++ main.js
@@ -0,0 +1,48 @@
+import './style.css'
+import SwaggerUI from 'swagger-ui'
+import SwaggerUIStandalonePreset from 'swagger-ui-dist/swagger-ui-standalone-preset';
+
+const ui = SwaggerUI({
+	dom_id: "#swagger-ui",
+  urls: [
+    {
+      "name": "archive",
+	    "url": "./schemas/archivage/openapi.yaml"
+    }, {
+      "name": "coherence",
+	    "url": "./schemas/coherence/openapi.yaml"
+    }, {
+      "name": "depot",
+	    "url": "./schemas/depot/openapi.yaml"
+    }, {
+      "name": "instruction",
+	    "url": "./schemas/instruction/openapi.yaml"
+    }, {
+      "name": "repertoire",
+	    "url": "./schemas/repertoire/openapi.yaml"
+    }, {
+      "name": "simulation",
+	    "url": "./schemas/simulation/openapi.yaml"
+    }
+  ],
+  layout: 'StandaloneLayout',
+  presets: [
+    SwaggerUI.presets.apis,
+    SwaggerUIStandalonePreset
+  ],
+  plugins: [
+    SwaggerUI.plugins.DownloadUrl
+  ]
+});
+
+ui.initOAuth({
+  clientId: "your-client-id",
+  clientSecret: "your-client-secret-if-required",
+  realm: "your-realms",
+  appName: "your-app-name",
+  scopeSeparator: " ",
+  scopes: "openid profile email phone address",
+  additionalQueryStringParams: {},
+  useBasicAuthenticationWithAccessCodeGrant: false,
+  usePkceWithAuthorizationCodeGrant: false
+});
